245840111b84f045b4b7579935c5f3e6022c8909
jag3921 <jag3921@rit.edu>
2021-12-05 16:35:37 -0500

    beta

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const unirest = require("unirest");
+const path = require('path');
+const API_KEY = "ee58002a-d577-452a-b2d2-e9b1d7fc4626";
+
+// serve static files from React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//Get api url
+app.get('/api/associations/:word', (req, res) => {
+	const word = req.params.word;
+	const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
+	.then(response => {
+		const results = response.body.response[0].items || []; // grab array of results
+		console.log(`Num results=${results.length}`);
+		res.json(results);
+	})
+	.catch(error => {
+		console.log(`error=${error}`);
+		res.json({status:"Error", message: `${error}`});
+	});
+});
+// Any other url
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+
+  // Start up app
+app.listen(port, () => {
+  console.log(`word-app listening on port ${port}`);
+});
